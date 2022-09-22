317733811aca2b120dad08fb7c94f369c42f74a1
Hud Wahab <hudwahab@gmail.com>
2022-09-22 15:50:27 -0600

    normal params in config

--- src/config.py
+++ src/config.py
@@ -5,3 +5,11 @@ from src.IET7600PlusControl import IET7600Plus
 # ptc10 = PTC10Control('169.254.106.13')
 # ezt = EZTControl('169.254.106.15')
 iet = IET7600Plus()
+
+# normal params
+iet.set_frequency(500)
+iet.set_primary_param('CS')
+iet.set_num_avg(1)
+
+# sweep params
+# iet.sweep_enable('OFF')
